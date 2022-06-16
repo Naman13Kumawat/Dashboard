88d6d450500fbb4bea8af926b805c2a665f727ed
Naman13Kumawat <naman.kumawat07@gmail.com>
2022-06-16 11:34:48 +0530

    Add Structure and Sidebar

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,7 @@
+import "./Main.scss"
+
+export default function Main() {
+  return (
+    <div className="main_container">Main Component</div>
+  )
+}
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,90 @@
+import "./Sidebar.scss"
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import PollIcon from "@mui/icons-material/Poll";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+
+          <p className="title">LISTS</p>
+
+          <li>
+            <PersonOutlineIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+
+          <p className="title">USEFUL</p>
+
+          <li>
+            <PollIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+
+          <p className="title">SERVICE</p>
+
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+
+          <p className="title">USER</p>
+
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="bottom">
+        <div className="colorOptions"></div>
+        <div className="colorOptions"></div>
+      </div>
+    </div>
+  );
+}
